b0e2e63f18cdece1d918af39f35764478139f6a4
gaconz <gaconz.learn@gmail.com>
2024-02-27 14:39:21 +0700

    Init sfg-pet-clinic practice

---
+++ src/main/java/loclt/sfp/sfgpetclinicpractice/SfgPetClinicPracticeApplication.java
@@ -0,0 +1,13 @@
+package loclt.sfp.sfgpetclinicpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/loclt/sfp/sfgpetclinicpractice/SfgPetClinicPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package loclt.sfp.sfgpetclinicpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
